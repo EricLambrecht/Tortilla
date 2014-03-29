862f4402253d3e05cdcec7d9d5bd258730f58c32
Eric <ericlambrecht.hh@gmail.com>
2014-03-29 20:43:14 +0100

    Initial FFmpeg process

---
+++ Tortilla/FFmpegProcess.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Makhani.Tortilla
+{
+	public class FFmpegProcess
+	{
+		public List<string> Output { get; }
+		protected Process process;
+
+		public FFmpegProcess ()
+		{
+		}
+
+		public void Run(string arguments) 
+		{
+			process = new Process();
+			// Configure the process using the StartInfo properties.
+			process.StartInfo.FileName = "ffmpeg";
+			process.StartInfo.Arguments = arguments;
+			process.StartInfo.CreateNoWindow = true;
+			process.StartInfo.UseShellExecute = false;
+			process.OutputDataReceived += OnDataReceived;
+			process.Start();
+			process.WaitForExit ();// Waits here for the process to exit.
+		}
+
+		public void CaptureLinuxScreen () 
+		{
+			string args = "-video_size 1024x768 -framerate 25 -f x11grab -i :0.0+100,200 -f alsa -ac 2 -i pulse output.flv";
+			Run (args); 
+		}
+
+		public void CaptureWindowsScreen () 
+		{
+			string args = "-f dshow -i video=\"UScreenCapture\":audio=\"Stereo Mix\" output.flv";
+			Run (args); 
+		}
+
+		public void StreamToUrl(string url, Resolution inputResolution, Resolution outputResolution) 
+		{
+			// ffmpeg -f dshow -i video="screen-capture-recorder":audio="Stereo Mix (IDT High Definition)" -vcodec libx264 -preset ultrafast -tune zerolatency -r 10 -async 1 -acodec libmp3lame -ab 24k -ar 22050 -bsf:v h264_mp4toannexb -maxrate 750k -bufsize 3000k -f mpegts udp://192.168.5.215:48550
+
+			string args = 
+				string.Format(
+					"-f x11grab -s {0} -r 15 -i :0.0 -c:v libx264 -preset fast -pix_fmt yuv420p -s {1} -threads 0 -f flv \"{2}\"", 
+					inputResolution,
+					outputResolution,
+					url
+				);
+			Run(args);
+		}
+			
+		// This function is windows only!
+		public void ListAudioDevices() 
+		{
+			//ffmpeg -list_devices true -f dshow -i dummy
+			string args = "-list_devices true -f dshow -i dummy";
+			Run (args);
+		}
+
+		protected void OnDataReceived(object sender, DataReceivedEventArgs received) 
+		{
+			Output.Add (received.Data);
+		}
+
+		public void End() 
+		{
+			process.Kill ();
+		}
+	}
+
+	public struct Resolution {
+		// In Pixels
+		int Width;
+		int Height;
+
+		public override string ToString() {
+			return string.Format ("{0}x{1}", Width, Height);
+		}
+	}
+}
+
---
+++ Tortilla/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Makhani
+{
+	public static class Utilities
+	{
+		public enum OS
+		{
+			Win,
+			Linux,
+			Mac
+		}
+
+		public static OS GetOS()
+		{
+			switch (Environment.OSVersion.Platform)
+			{
+			case PlatformID.Unix:
+				// Well, there are chances MacOSX is reported as Unix instead of MacOSX.
+				// Instead of platform check, we'll do a feature checks (Mac specific root folders)
+				if (Directory.Exists("/Applications")
+					& Directory.Exists("/System")
+					& Directory.Exists("/Users")
+					& Directory.Exists("/Volumes"))
+					return OS.Mac;
+				else
+					return OS.Linux;
+
+			case PlatformID.MacOSX:
+				return OS.Mac;
+
+			default:
+				return OS.Win;
+			}
+		}
+	}
+}
+
